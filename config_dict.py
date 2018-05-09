8995a45f77a6966d5cbe16b5f03e711e89bc8d2b
seinfeld1311 <ich@ericfries.de>
2018-05-09 22:35:41 +0200

    Add files via upload

---
+++ config_dict.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = "Eric Fries"
+__copyright__ = "Copyright 2018, Eric Fries"
+__license__ = "GPL"
+__version__ = "2.0"
+__maintainer__ = "Eric Fries"
+__email__ = "ich@ericfries.de"
+__status__ = "Production"
+
+
+"""
+This is the config file for the PictSort script.
+endungen_exif -> Exif readable media files (Exifread)
+endungen_cfreated -> Not Exif readble media files. It reades the created date from the file
+-> key:value == file:folder
+
+"""
+
+endungen_exif = {
+    "jpeg": "jpeg",
+    "jpg": "jpeg",
+    "rw2": "raw",
+}  # -> Media with readable exif files (raw/ Pictures) File extensions from the media files in front as key and the folder to be crated as value
+
+endungen_created = {
+    "mp4": "mov",
+    "mov": "mov",
+}  # -> Media without readable Exif Files / File extensions from the media files in front as key and the folder to be crated as value
